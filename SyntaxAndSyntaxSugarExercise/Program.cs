d9417c5dc8459c7ee1a4f628d7cfbbba8d6e2729
Maverick Grayer <maverick72032@gmail.com>
2023-11-08 11:57:57 -0500

    Assignment complete

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 20;
+            var response = (answer < 9) ?   $"{answer} is less than 9? That explains everything!" : 
+                                            $"{answer} is greater than or equal to 9? Swolebrain tricked me!"; //changed nine to 9, looks better when printed to console
+            Console.WriteLine(response);
         }
     }
 }
